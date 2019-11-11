7273b528cc2f05810f8cee5292093e0876f123bd
Omar Lopez <omar.enrique1211@gmail.com>
2019-11-11 17:11:12 +0200

    Creating a very fake user model

---
+++ models/users.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define("Users", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
